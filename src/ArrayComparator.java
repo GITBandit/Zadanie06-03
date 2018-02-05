94060dcb97480cd9ba61dc979c2d84923974e660
Dominik Jakubowski <dominikjakubowski@gmail.com>
2018-02-05 16:24:19 +0100

    Condition in the compare method amended so it stops when numbers do not match.

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -2,17 +2,16 @@ public class ArrayComparator {
 
 
     boolean compare(int [][] tab1, int [][] tab2){
-        boolean check = true;
 
         for(int i = 0; i<tab1.length;i++){
             for(int j = 0; j<tab1[i].length;j++){
                 if (tab1[i][j]!=tab2[i][j]){
-                    check = false;
+                    return false;
                 }
             }
         }
 
-        return check;
+        return true;
 
     }
 
